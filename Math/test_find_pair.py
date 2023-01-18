d28e7326d8a44f94fe9c0bf6a851741aeed00c24
artialpha <arturszafraniak6@gmail.com>
2023-01-18 16:57:25 +0100

    test for a function that finds a pair of numbers that add up to a given sum is completed (it works for both even and odd numbers)

--- Math/test_find_pair.py
+++ Math/test_find_pair.py
@@ -1,47 +1,41 @@
 from unittest import TestCase
 from random import sample, randint, shuffle
-from Math.find_pair import find_pair
+from Math.Find_pair import find_pair
 
 
 class Test(TestCase):
 
     def test_find_pair(self):
-        nb_tests = 100
-
-        def generate_pairs_even(s):
-            """
-            I want to generate a list of numbers which have only one pair of numbers that add up to a given sum
-
-            :param s:
-            sum which is supposed to be obtained by adding two numbers that can be found in a list
-            :return:
-            list of numbers which contains one pair that can be added to get a desired sum
-            """
+        def generate_list_with_sums(s, range_list, size_list):
             half = s/2
 
-            ls = sorted(sample(range(3*nb_tests), nb_tests))
+            # random distances from a half of the sum
+            ls = sorted(sample(range(range_list), size_list))
 
             ls_pairs = []
             for index, nb in enumerate(ls):
                 if index % 2 == 0:
-                    res = half + nb
+                    res = round(half + nb)
                 else:
-                    res = half - nb
+                    res = round(half - nb)
                 ls_pairs.append(res)
 
             nb = ls[0]
             while nb in ls:
-                nb = randint(0, 3*nb_tests)
-            pair = [half-nb, half+nb]
-
+                nb = randint(0, range_list) if s % 2 == 0 else randint(0, range_list) + 0.5
+            pair = [int(half-nb), int(half+nb)]
+            print(f'this is pair: {pair}')
             ls_pairs.extend(pair)
             shuffle(ls_pairs)
             ls_pairs.sort()
 
             return ls_pairs
 
-        s = 1000
+        nb_tests = 100
         for _ in range(nb_tests):
-            ls = generate_pairs_even(s)
+            s = randint(100, 1000)
+            ls = generate_list_with_sums(s=s, range_list=int(s/3), size_list=int(s/4))
+            print(ls)
             a_index, b_index = find_pair(ls, s)
+            print(ls[a_index], ls[b_index], f'sum: {ls[a_index] + ls[b_index]}')
             self.assertEqual(s, ls[a_index] + ls[b_index])
